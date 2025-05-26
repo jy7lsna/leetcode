f4f8191401a7bd59d77e75f7d1376a486b04dd21
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-05-26 22:56:21 +0530

    Time: 8 ms (79.24%) | Memory: 18 MB (32.59%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wordToDict(self, word):
+        d ={}
+        for letter in word:
+            d[letter] = d.get(letter, 0) + 1
+        return d
+    def isAnagram(self, s: str, t: str) -> bool:
+        return self.wordToDict(s) == self.wordToDict(t)

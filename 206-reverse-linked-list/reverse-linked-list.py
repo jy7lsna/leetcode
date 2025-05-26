f400a1aaeda79ec711ab5968813a77fbc09db15a
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-05-27 01:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (38.03%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,6 +6,12 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         return self.reverseListIterative(head)
+        return self.reverseListRec(head, None)
+    
+    def reverseListRec(self, curr: Optional[ListNode], prev:Optional[ListNode]) -> Optional[ListNode]:
+        next_node = curr.next
+        curr.next = prev
+        return self.reverseListRec(next_node, prev)
 
     def reverseListIterative(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None

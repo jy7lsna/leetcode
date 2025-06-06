e8a1d835397030f00d2584b608cdb63725647fa3
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-06-06 19:12:17 +0530

    Time: 36 ms (80.63%) | Memory: 54.7 MB (5.42%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -2,8 +2,12 @@
  * @return {Function}
  */
 const createHelloWorld = function(){
-    return (...args) => "Hello World";
+    return function(...args){
+        return "Hello World";
+    }
 }
+
+
 
 /**
  * const f = createHelloWorld();

6f79affc92c429657ddef0a1c58f0935101075ce
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-06-06 20:46:39 +0530

    Time: 50 ms (6.90%) | Memory: 52.2 MB (95.86%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -4,20 +4,6 @@
 const createHelloWorld = function(){
     return () => "Hello World"
 }
-
-// traditional func
-// const add = function(a, b){
-//     return a+b;
-// }
-
-// arrow
-// const add =(a,b) => a+b;
-
-// const button = document.querySelector('button');
-// button.addEventListener('click', () => {
-//     console.log(this)
-// })
-
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"

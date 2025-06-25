fd5bfba1e06ee05cf7887aae71a4211767aee88c
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-06-25 20:09:54 +0530

    Time: 4 ms (73.88%) | Memory: 20.9 MB (21.54%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -5,34 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.node_to_max_depth = {None: 0}
-
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.populateMaxDepth(root)
-        if not root:
-            return 0
+        res = 0
         
-        # case1 : go thru the root
-        left_max_depth = self.node_to_max_depth[root.left]
-        right_max_depth = self.node_to_max_depth[root.right]
-        case1 = left_max_depth + right_max_depth
-        # case2: only consider left
-        case2 = self.diameterOfBinaryTree(root.left)
-        # case3 only right
-        case3 = self.diameterOfBinaryTree(root.right)
+        def dfs(curr):
+            if not curr:
+                return 0
 
-        return max(case1, case2, case3)
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+            
+            nonlocal res
+            res = max(res, left + right)
+            return 1 + max(left, right)
 
-    def populateMaxDepth(self, root):
-        if root in self.node_to_max_depth:
-            return
+        dfs(root)
+        return res
         
-        self.populateMaxDepth(root.left)
-        self.populateMaxDepth(root.right)
-
-        self.node_to_max_depth[root] = 1 + max(
-            self.node_to_max_depth[root.left],
-            self.node_to_max_depth[root.right],
-        )
-

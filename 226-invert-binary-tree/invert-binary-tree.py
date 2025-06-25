b624e5b5dfffa0d5ffc1246a854d9b2e51854cb2
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-06-25 18:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.14%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -6,12 +6,39 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # bfs
+        # if not root:
+        #     return None
+        
+        # queue = deque([root])
+        # while queue:
+        #     node = queue.popleft()
+        #     node.left, node.right = node.right, node.left
+        #     if node.left:
+        #         queue.append(node.left)
+        #     if node.right:
+        #         queue.append(node.right)
+        # return root
+
+        # dfs
         if not root:
             return None
-        
         temp = root.left
         root.left = root.right
         root.right = temp
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+        # iterative dfs
+        # if not root:
+        #     return None
+        
+        # stack = [root]
+        # while stack:
+        #     node = stack.pop()
+        #     node.left, node.right = node.right, node.left
+        #     if node.left:
+        #         stack.append(node.left)
+        #     if node.right:
+        #         stack.append(node.right)

d275d0068dad7654ac467ce370f0b1050b50da61
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-07-07 21:56:15 +0530

    Time: 38 ms (43.21%) | Memory: 19.4 MB (46.60%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -4,7 +4,7 @@ class Solution:
         pac, atl = set(), set()
 
         def dfs(r, c, visit, prevHeight):
-            if((r, c) in visit or 
+            if ((r, c) in visit or 
                 r < 0 or c < 0 or r == ROWS or c == COLS or
                 heights[r][c] < prevHeight):
                 return
@@ -17,11 +17,11 @@ class Solution:
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
             dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
-        
+
         for r in range(ROWS):
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS - 1, atl, heights[r][COLS - 1])
-        
+
         res = []
         for r in range(ROWS):
             for c in range(COLS):

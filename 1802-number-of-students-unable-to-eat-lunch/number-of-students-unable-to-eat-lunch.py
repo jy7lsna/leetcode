43616a31bc06df0b802368700496dd488951003d
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-07-26 20:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (63.41%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,17 +1,8 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        students = deque(students)
-        sandwiches = deque(sandwiches)
-
-        attempts = 0
-
-        while students and attempts < len(students):
-            if(students[0] == sandwiches[0]):
-                students.popleft()
-                sandwiches.popleft()
-                attempts = 0
-            else:
-                students.append(students.popleft())
-                attempts += 1 
-        
-        return len(students)
+        count = Counter(students)
+        for s in sandwiches:
+            if count[s] == 0:
+                break
+            count[s] -= 1
+        return count[0] + count[1]

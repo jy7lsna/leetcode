0181d8be66141f187a457c351084e1df6264a6d1
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-08-22 19:22:29 +0530

    Time: 630 ms (52.78%) | Memory: 25.6 MB (35.63%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -3,7 +3,7 @@ class Solution:
         if len(nums) <= 1:
             return nums
         
-        mid = len(nums)//2
+        mid = len(nums) // 2
         left = self.sortArray(nums[:mid])
         right = self.sortArray(nums[mid:])
 
@@ -23,4 +23,21 @@ class Solution:
         res.extend(left[i:])
         res.extend(right[j:])
         return res
-            
+
+
+    def quickSort(self, nums, low, high):
+        if low < high:
+            pi = self.partition(nums, low, high)
+            self.quickSort(nums, 0, pi-1)
+            self.quickSort(nums, pi+1, high)
+    
+    def partition(self, nums, low, high):
+        pivot = len(nums) - 1
+        i = low - 1
+
+        for j in range(low, high):
+            if nums[j] <= pivot:
+                nums[i], nums[j] = nums[j], nums[i]
+        nums[i+1], nums[high] = nums[high], nums[i+1]
+        return i + 1
+

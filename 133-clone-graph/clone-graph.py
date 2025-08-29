02bd98f22ae09d2a1cf31c3e861923cccf2be66a
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-08-29 13:23:30 +0530

    Time: 46 ms (38.48%) | Memory: 18.3 MB (16.42%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,16 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-        oldToNew = {}
+        
+        clones = {node: Node(node.val)}
+        q = deque([node])
 
-        def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
-            copy = Node(node.val)
-            oldToNew[node] = copy
-            for n in node.neighbors:
-                copy.neighbors.append(dfs(n))
-            return copy
-        return dfs(node)
+        while q:
+            curr = q.popleft()
+            for nei in curr.neighbors:
+                if nei not in clones:
+                    clones[nei] = Node(nei.val)
+                    q.append(nei)
+                clones[curr].neighbors.append(clones[nei])
+        return clones[node]

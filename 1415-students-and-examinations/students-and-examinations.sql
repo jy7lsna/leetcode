d69c3b49d48a6130157bbd8dd123c582116f9df1
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-12-14 19:19:10 +0530

    Time: 966 ms (40.11%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -5,6 +5,5 @@ cross join subjects sub
 left join examinations e 
 on s.student_id = e.student_id
 and sub.subject_name = e.subject_name
-
 group by s.student_id, s.student_name, sub.subject_name
 order by s.student_id, sub.subject_name;

86e33fdd28b4aa615282bd8148f045f5be634c6e
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-12-19 18:23:37 +0530

    Time: 20 ms (25.28%) | Memory: 22 MB (36.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,7 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
-
         for s in strs:
             freq = [0] * 26
             for ch in s:
@@ -11,9 +10,3 @@ class Solution:
         return list(groups.values())
 
 
-# eat = e-1, a-1, t-1
-# tea = t-1, e-1, a-1
-# tan = t-1, a-1, n-1
-# ate = a-1, t-1, e-1
-# nat = n-1, a-1, t-1
-# bat = b-1, a-1, t-1

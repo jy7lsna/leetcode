3f3b1de95e2db70a819de59795da326a6a7d0b69
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2025-12-31 14:56:50 +0530

    Time: 16 ms (22.76%) | Memory: 17.5 MB (97.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,16 +2,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
+        
         freq = [0] * 26
         for i in range(len(s)):
             freq[ord(s[i]) - ord('a')] += 1
             freq[ord(t[i]) - ord('a')] -= 1
-
+        
         for count in freq:
             if count != 0:
                 return False
         return True
-
-
-

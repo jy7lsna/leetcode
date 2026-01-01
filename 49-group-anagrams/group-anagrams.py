f03658e6dbb19a2fb2b2d6b4dcf659bb508f90c0
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2026-01-01 19:50:59 +0530

    Time: 21 ms (23.44%) | Memory: 21.9 MB (31.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,7 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
-
         for word in strs:
             freq = [0] * 26
             for c in word:

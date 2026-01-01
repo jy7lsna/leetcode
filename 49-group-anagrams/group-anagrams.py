cd13b2ce395eddd6d030f54f45aee89a643e31bb
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2026-01-01 19:51:56 +0530

    Time: 19 ms (37.78%) | Memory: 21.8 MB (32.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,8 +5,9 @@ class Solution:
             freq = [0] * 26
             for c in word:
                 freq[ord(c) - ord('a')] += 1
-            groups[tuple(freq)].append(word)
-        return list((groups).values())
+            key = tuple(freq)
+            groups[key].append(word)
+        return list(groups.values())
 
 
 

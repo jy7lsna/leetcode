f0e11331a1e1d0d9789543a6b54e24b6b10e798e
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2026-01-02 21:40:18 +0530

    Time: 10 ms (98.09%) | Memory: 17.5 MB (89.00%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,28 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n, m = len(s1), len(s2)
-        if n > m:
+        if len(s1) > len(s2):
             return False
-        
-        need = [0] * 26 
-        window = [0] * 26
-        
-        for ch in s1:
-            need[ord(ch) - ord('a')] += 1
 
+        freq = [0] * 26
+        for c in s1:
+            freq[ord(c) - ord('a')] += 1
+        
+        n = len(s1)
+        window = [0] * 26
         for i in range(n):
             window[ord(s2[i]) - ord('a')] += 1
-
-        if need == window:
+        
+        if window == freq:
             return True
         
-        for r in range(n, m):
+        for r in range(n, len(s2)):
             window[ord(s2[r]) - ord('a')] += 1
             window[ord(s2[r-n]) - ord('a')] -= 1
 
-            if window == need:
+            if window == freq:
                 return True
         
         return False
-        
         

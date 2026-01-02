d5428e725fa3d7ed6caa9562f6d8c3ce8f92fe63
Jyolsna <121849188+jy7lsna@users.noreply.github.com>
2026-01-02 11:36:40 +0530

    Time: 4 ms (56.20%) | Memory: 17.6 MB (78.43%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
         boxes = [set() for _ in range(9)]
 
         for r in range(9):
@@ -9,14 +9,10 @@ class Solution:
                 val = board[r][c]
                 if val == ".":
                     continue
-                
                 box_idx = (r//3) * 3 + (c//3)
-                if (val in rows[r] or val in cols[c] or val in boxes[box_idx]):
+                if (val in row[r] or val in col[c] or val in boxes[box_idx]):
                     return False
-                
-                rows[r].add(val)
-                cols[c].add(val)
+                row[r].add(val)
+                col[c].add(val)
                 boxes[box_idx].add(val)
-        return True 
-
-        
+        return True
